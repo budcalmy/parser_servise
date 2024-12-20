98e392153c1c989521352aceee760aca08df25bc
budcalmy <149785455+budcalmy@users.noreply.github.com>
2024-12-20 14:00:52 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
 
 from my_parser import CppInterpreter
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class TextRequest(BaseModel):
     code: str
